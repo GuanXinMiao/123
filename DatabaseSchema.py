285a7f8067331cb4210cfdb1efc2cb4248376a9c
GuanXinMiao <46599878+GuanXinMiao@users.noreply.github.com>
2019-12-16 15:39:07 +1300

    Update and rename schema.py to DatabaseSchema.py

--- DatabaseSchema.py
+++ DatabaseSchema.py
@@ -1,8 +1,8 @@
 """
 Author : Guan Xin Miao
+Date: 13/12/2019
 Output: https://docs.google.com/spreadsheets/d/1-fNBj4V266e6D5HNGRNZ6Kfi02kVXfRsnlvEa7PNflQ/edit?ts=5df1ab95#gid=1236490588
-Purpose: store
- all database variable information to google sheet document
+Purpose: store all database variable information to google sheet document
 """
 import pandas as pd
 import pymysql
@@ -15,6 +15,7 @@ from df2gspread import df2gspread as d2g
 def store_data_to_google_sheet(database_list, row_nums, sheet_name, credentials, cursor):
 
 	for i in range(len(database_list)):
+
 		sqlStr = """
 		SHOW FULL columns FROM {}
 		""".format(database_list[i])
@@ -36,13 +37,13 @@ def store_data_to_google_sheet(database_list, row_nums, sheet_name, credentials,
 		col_letter = ['E', 'H', 'K', 'G', 'N']
 
 		for col in range(len(data_cols)):
-			d2g.upload(df = data_cols[col], gfile = '1-fNBj4V266e6D5HNGRNZ6Kfi02kVXfRsnlvEa7PNflQ', start_cell = col_letter[col] + row_nums[i], wks_name =  sheet_name, col_names = False, row_names = False, clean = False, credentials = creds, df_size = False)
+			d2g.upload(df = data_cols[col], gfile = '1-fNBj4V266e6D5HNGRNZ6Kfi02kVXfRsnlvEa7PNflQ', start_cell = col_letter[col] + row_nums[i], wks_name =  sheet_name, col_names = False, row_names = False, clean = False, credentials = credentials, df_size = False)
 			print(col)
 
 		print(database_list[i] + " finished upload")
 
 def main():
-	conn = pymysql.connect(host='funnow-prod-go-20190514-for-metabase.cqajv42imxem.ap-northeast-1.rds.amazonaws.com', port=3306, user='funnow', passwd='b5D_C4-7f', db='FunNow_V2')
+	conn = pymysql.connect(host='funnow-prod-go-20190514-for-metabase.cqajv42imxem.ap-northeast-1.rds.amazonaws.com', port=3306, user='funnow', passwd='********', db='FunNow_V2')
 	cursor = conn.cursor()
 
 	#set google sheet upload
@@ -50,10 +51,12 @@ def main():
 	creds = ServiceAccountCredentials.from_json_keyfile_name("trySheets-708d0bd4d3dc.json", scope)
 	client = gspread.authorize(creds)
 
-	database_list = ['Resource', 'ResourceGroup']
-	row_nums = ['6', '14']
+	database_list = ['IndustryType']
+	#
+	#
+	row_nums = ['65']
 
-	sheet = 'Resource'
+	sheet = 'Branch'
 
 	store_data_to_google_sheet(database_list, row_nums, sheet, creds, cursor)
 
