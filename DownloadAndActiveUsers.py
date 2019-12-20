a5a6225dbe3b230003dd338c9f0a0b653522fed5
GuanXinMiao <46599878+GuanXinMiao@users.noreply.github.com>
2019-12-20 17:00:13 +1300

    Update DownloadAndActiveUsers.py

--- DownloadAndActiveUsers.py
+++ DownloadAndActiveUsers.py
@@ -1,5 +1,3 @@
-# FunNow
-funnow programs
 """
 Author: Guan Xin Miao
 Date: 17/12/2019
@@ -17,6 +15,7 @@ import datetime
 import os
 import smtplib
 import gspread
+import math
 from oauth2client.service_account import ServiceAccountCredentials
 from df2gspread import df2gspread as d2g
 from email.mime.multipart import MIMEMultipart
@@ -24,6 +23,8 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.utils import formatdate
 from email import encoders
+
+#-------------------- dataFrame from sql dataframe functions --------------------------------------#
 
 def get_dataFrame_for_Register_sheet(search_start_date, search_end_date, cursor):
 	query_string = """
@@ -206,7 +207,7 @@ def get_dataFrame_for_FirstBuy_sheet(search_start_date, search_end_date, cursor)
 
 	return df
 
-##############################################################################################################################################################################
+#-------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 def send_mail(send_from, send_to, subject, text, filepath, password):
     msg = MIMEMultipart()
@@ -228,24 +229,27 @@ def send_mail(send_from, send_to, subject, text, filepath, password):
     text = msg.as_string()
     server.sendmail(send_from, send_to, text)
     server.quit()
-##############################################################################################################################################################################
-def store_data_register(dataframe, creds, sheet):
+#-------------------------------------------- store data functions --------------------------------------------------------------------------------------#
+def store_data_register(dataframe, creds, date_diff):
 
 	sheet_name = "Register"
-	start_row = str(len(sheet.get_all_records()) - 5)
+	start_row = str(date_diff - 11)
 	d2g.upload(df = dataframe, gfile = '1skrKAOLZLPoul59kgjbY6jQwE60VxVCOcIwG2fZJCXo', start_cell = "A" + start_row, wks_name = sheet_name , col_names = False, row_names = False, clean = False, credentials = creds, df_size = False)
+	print(sheet_name + " finished!")
 
-def store_data_DAU(dataframe, creds, sheet):
+def store_data_DAU(dataframe, creds, date_diff):
 
 	sheet_name = 'DAU'
-	start_row = str(len(sheet.get_all_records()) - 5)
+	start_row = str(date_diff - 11)
 	d2g.upload(df = dataframe, gfile = '1skrKAOLZLPoul59kgjbY6jQwE60VxVCOcIwG2fZJCXo', start_cell = "A" + start_row, wks_name = sheet_name , col_names = False, row_names = False, clean = False, credentials = creds, df_size = False)
+	print(sheet_name + " finished!")
 
-def store_data_WAU(dataframe, creds, sheet):
+def store_data_WAU(dataframe, creds, week_diff):
 
 	sheet_name = 'WAU'
-	start_row = str(len(sheet.get_all_records()) + 1)
+	start_row = str(week_diff - 1)
 	d2g.upload(df = dataframe, gfile = '1skrKAOLZLPoul59kgjbY6jQwE60VxVCOcIwG2fZJCXo', start_cell = "A" + start_row, wks_name = sheet_name , col_names = False, row_names = False, clean = False, credentials = creds, df_size = False)
+	print(sheet_name + " finished!")
 
 def store_data_MAU(dataframe, creds, sheet, new_month):
 
@@ -254,44 +258,56 @@ def store_data_MAU(dataframe, creds, sheet, new_month):
 	if new_month: start_row = str(len(sheet.get_all_records()) + 1)
 	else:start_row = str(len(sheet.get_all_records()))
 	d2g.upload(df = dataframe, gfile = '1skrKAOLZLPoul59kgjbY6jQwE60VxVCOcIwG2fZJCXo', start_cell = "A" + start_row, wks_name = sheet_name , col_names = False, row_names = False, clean = False, credentials = creds, df_size = False)
+	print(sheet_name + " finished!")
 
-def store_data_firstBuy(dataframe, creds, sheet):
+def store_data_firstBuy(dataframe, creds, week_diff):
 	sheet_name = 'FirstBuy'
-	start_row = str(len(sheet.get_all_records()) + 1)
+	start_row = str(week_diff - 1)
 	d2g.upload(df = dataframe, gfile = '1skrKAOLZLPoul59kgjbY6jQwE60VxVCOcIwG2fZJCXo', start_cell = "A" + start_row, wks_name = sheet_name , col_names = False, row_names = False, clean = False, credentials = creds, df_size = False)
+	print(sheet_name + " finished!")
 
-###############################################################################################################################################################################
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 def main():
+	#set dates
+	project_start_date = date(2019, 1, 1)
 	current_date = date.today()
 	first = current_date.replace(day=1)
 	last_month = first - datetime.timedelta(days=1)
 	last_month_first = '"' + str(last_month.year) + '-' + str(last_month.month) + '-' + '1' + '"'
 	yesterday = current_date - datetime.timedelta(days = 1)
+	date_diff = (yesterday - project_start_date).days
+	week_diff = math.ceil(date_diff/7)
 	two_weeks_before = current_date - datetime.timedelta(weeks = 2)
 	is_new_month = (yesterday.day < 7)
 	yesterday = '"' + str(yesterday) + '"'
 
 	two_weeks_before = '"' + str(two_weeks_before) + '"'
+	#set mysql connections
 
-	connect = pymysql.connect(host='funnow-prod-go-20190514-for-metabase.cqajv42imxem.ap-northeast-1.rds.amazonaws.com', port=3306, user='funnow', passwd='*********', db='FunNow_V2')
+	connect = pymysql.connect(host='funnow-prod-go-20190514-for-metabase.cqajv42imxem.ap-northeast-1.rds.amazonaws.com', port=3306, user='funnow', passwd='********', db='FunNow_V2')
 	cursor = connect.cursor()
+	#store dataFrame to a python list
 
-	df_list = [get_dataFrame_for_Register_sheet(two_weeks_before, yesterday, cursor), get_dataFrame_for_DAU_sheet(two_weeks_before, yesterday, cursor),\
-	get_dataFrame_for_WAU_sheet(two_weeks_before, yesterday, cursor), get_dataFrame_for_MAU_sheet(last_month_first, yesterday, cursor),\
-	 get_dataFrame_for_FirstBuy_sheet(two_weeks_before, yesterday, cursor)]
+	df_list = [get_dataFrame_for_Register_sheet(two_weeks_before, yesterday, cursor),\
+	get_dataFrame_for_DAU_sheet(two_weeks_before, yesterday, cursor),\
+	get_dataFrame_for_WAU_sheet(two_weeks_before, yesterday, cursor),\
+	get_dataFrame_for_MAU_sheet(last_month_first, yesterday, cursor),\
+	get_dataFrame_for_FirstBuy_sheet(two_weeks_before, yesterday, cursor)]
 
+	#set google sheet information
 	scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 	creds = ServiceAccountCredentials.from_json_keyfile_name("trySheets-708d0bd4d3dc.json", scope)
 	client = gspread.authorize(creds)
+	sheet_file = client.open("DownloadAndActiveUsers")
 
 	#store data to google sheet
-	sheet_file = client.open("DownloadAndActiveUsers")
-	store_data_register(df_list[0], creds, sheet_file.get_worksheet(7))
-	store_data_DAU(df_list[1], creds, sheet_file.get_worksheet(8))
-	store_data_WAU(df_list[2], creds, sheet_file.get_worksheet(9))
+	store_data_register(df_list[0], creds, date_diff)
+	store_data_DAU(df_list[1], creds, date_diff)
+	store_data_WAU(df_list[2], creds, week_diff)
 	store_data_MAU(df_list[3], creds, sheet_file.get_worksheet(10), is_new_month)
-	store_data_firstBuy(df_list[4], creds, sheet_file.get_worksheet(11))
+	store_data_firstBuy(df_list[4], creds, week_diff)
+	print("Program finished")
 
 if __name__ == '__main__':
 	main()
